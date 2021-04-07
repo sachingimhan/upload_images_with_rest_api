6138594da585ccfc6175a1b0caf91630179f16a4
sachingimhan <sachingimhan8@gmail.com>
2021-04-07 14:48:04 +0530

    ImagesUploads:upload images with REST Api

---
+++ src/main/java/lk/sample/AppInit.java
@@ -0,0 +1,11 @@
+package lk.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInit {
+    public static void main(String[] args) {
+        SpringApplication.run(AppInit.class, args);
+    }
+}
---
+++ src/main/java/lk/sample/config/WebAppConfig.java
@@ -0,0 +1,7 @@
+package lk.sample.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebAppConfig {
+}
